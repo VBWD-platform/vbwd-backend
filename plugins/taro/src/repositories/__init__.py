10753cd0c7bd11ae1aecb4575a2d432c2b4737cc
dantweb <daniil.tkachev@gmail.com>
2026-03-16 18:50:36 +0100

    Revert "chore: remove plugin dirs from repo tracking (hosted in standalone repos)"
    This reverts commit 43e1aa3c9f53a60888fc0eb8e5d31c6c77b9a948.

---
+++ plugins/taro/src/repositories/__init__.py
@@ -0,0 +1,12 @@
+"""Taro plugin repositories."""
+from plugins.taro.src.repositories.arcana_repository import ArcanaRepository
+from plugins.taro.src.repositories.taro_session_repository import TaroSessionRepository
+from plugins.taro.src.repositories.taro_card_draw_repository import (
+    TaroCardDrawRepository,
+)
+
+__all__ = [
+    "ArcanaRepository",
+    "TaroSessionRepository",
+    "TaroCardDrawRepository",
+]
